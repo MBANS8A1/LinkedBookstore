b9de002739784acc7cd2918b3234bbef0225b550
MBANS8A1 <seanmbng@aol.com>
2024-11-01 15:01:10 +0000

    Added logic for middleware in Startup.cs and routing to root page and /pages.  Some middleware is commented out.

--- Sean.LinkedBookStore/Sean.LinkedBookStore/Program.cs
+++ Sean.LinkedBookStore/Sean.LinkedBookStore/Program.cs
@@ -1,6 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+//This code is for the new version of .NET8.0 where you do not need a Startup.cs file
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
-app.Run();
+//app.Run();
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Sean.LinkedBookStore
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => //webhostbuilder
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        
+    }
+}
+
---
+++ Sean.LinkedBookStore/Sean.LinkedBookStore/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Sean.LinkedBookStore
+{
+    public class Startup
+    {
+        //This method gets called at runtime. Use this method to add services to the container
+        //
+        public void ConfigurationServices(IServiceCollection service) //for dependencies used in the application are defined here
+        {
+
+        }
+
+        //This method gers called at runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) //for the http applications and using environment of the //application
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //app.Use(async (context,next) =>
+            //{
+            //    await context.Response.WriteAsync("A welcome from my first middleware.\n\n");
+            //    await next();
+            //    await context.Response.WriteAsync("A welcome from my first middleware response.\n\n");
+
+            //});
+
+            //app.Use(async (context,next) =>
+            //{
+            //    await context.Response.WriteAsync("A welcome from my second middleware.\n\n");
+            //    await next();
+            //    await context.Response.WriteAsync("A welcome from my second middleware response.\n\n");
+            //});
+
+            //app.Use(async (context,next) =>
+            //{
+            //    await context.Response.WriteAsync("A welcome from my third middleware.\n\n");
+            //    await next();
+            //    await context.Response.WriteAsync("A welcome from my third middleware response.\n\n");
+            //});
+
+            app.UseRouting(); //to enable routing and to map a URL to a particular resource
+            app.UseEndpoints(endpoints =>   //for setting up the default routing
+               {      //Map maps the url ("/" is the default url) to the resource
+                   endpoints.Map("/", async context =>
+                   {
+                       await context.Response.WriteAsync("Hello from linkedbookstore application\n\n");
+                   });
+            });
+            app.UseEndpoints(endpoints =>   //for setting up the route to pages
+            {      //Map maps the url "/pages"
+                endpoints.Map("/pages", async context =>
+                {
+                    await context.Response.WriteAsync("Hello pages\n\n");
+                });
+            });
+        }
+    }
+}
+   
